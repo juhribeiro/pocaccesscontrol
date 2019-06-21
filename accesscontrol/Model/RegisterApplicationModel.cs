80836663c9027a51753488997e46320d4b0be7b9
JuRibeiro <juliane.goncalves94@gmail.com>
2019-06-21 18:14:11 -0300

    valid required objects in register model

--- accesscontrol/Model/RegisterApplicationModel.cs
+++ accesscontrol/Model/RegisterApplicationModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace accesscontrol.Model
 {
@@ -9,7 +10,10 @@ namespace accesscontrol.Model
             this.Roles = new List<RoleModel>();
         }
 
+        [Required]
         public virtual ApplicationModel Application { get; set; }
+
+        [Required]
         public virtual List<RoleModel> Roles { get; set; }
     }
 }
